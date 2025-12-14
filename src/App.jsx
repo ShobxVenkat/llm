100c514169436aa08159273c487f336cd111396e
Shobhit Tiwari <officialshobhit108@gmail.com>
2025-12-14 12:42:13 +0530

    Update landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero ";
+import Hero from "./components/Hero";
 import Stats from "./components/Stats";
-import Services from "./components/Services ";
-import About from "./components/About ";
+import Services from "./components/Services";
+import About from "./components/About";
 import Careers from "./components/Careers";
 import CTA from "./components/CTA";
-import Footer from "./components/Footer ";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
